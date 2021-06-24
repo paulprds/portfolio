042affd12a478dde610ab38675696c0955734002
Paul <rpph3@hotmail.com>
2021-06-23 20:30:44 -0400

    My first Commit

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+import data from "../yourdata"
+
+const Header = () => {
+  return (
+    <div className="section" id="home">
+      <div className="container">
+        <div className="header-wrapper">
+          <Fade bottom>
+            <h2>
+              Hi, I'm {data.name}{" "}
+              <span role="img" aria-label="Emoji">
+                
+              </span>
+            </h2>
+          </Fade>
+          <Fade bottom cascade>
+            <div className="heading-wrapper">
+              <h1>
+                {data.headerTagline[0]
+                  ? data.headerTagline[0]
+                  : ""}
+              </h1>
+              <h1>
+                {" "}
+                {data.headerTagline[1]
+                  ? data.headerTagline[1]
+                  : ""}
+              </h1>
+              <h1>
+                {" "}
+                {data.headerTagline[2]
+                  ? data.headerTagline[2]
+                  : ""}
+              </h1>
+            </div>
+          </Fade>
+          <Fade bottom>
+            <p>{data.headerParagraph}</p>
+          </Fade>
+          <Fade bottom>
+            <a
+              href={`mailto:${
+                data.contactEmail ? data.contactEmail : "rpph777@gmail.com"
+              }`}
+              className="primary-btn"
+            >
+              rpph777@gmail.com
+            </a>
+          </Fade>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/yourdata.js
@@ -0,0 +1,194 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Paul",
+  headerTagline: [
+    //Line 1 For Header
+    "Building Digital Products",
+    //Line 2 For Header
+   "for your best Experience",
+    //Line 3 For Header
+    "and Usability",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "I'm an experienced software engineer who constantly seeks out innovative solutions to everyday problems. In my seven years in this industry, I've honed my analytical thinking and collaboration skills, and I love working with a team. I've also had the opportunity to serve as the software engineer lead for three projects with First Technology.",
+
+  //Contact Email
+  contactEmail: "rpph777@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Voixtek PWA app", //Project Title - Add Your Project Title Here
+      para:
+        "A multi platform progressive web app for singers, using Angular framework with the most cutting-edge technology to give the users a vocal training like never before.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i0.wp.com/jyroneparker.com/wp-content/uploads/2019/10/PWA-Logo-1.png?fit=1080%2C1080&ssl=1",
+      //Project URL - Add Your Project Url Here
+      url: "http://voixtek.com",
+    },
+    {
+      title: "International Project Binational Ecuador-Colombia", //Project Title - Add Your Project Title Here
+      para:
+        "International Project Binational Ecuador-Colombia: Project manager, to create a full stack Binational Information System.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.cmarix.com/blog/wp-content/uploads/2020/05/Black-Dash.png",
+      //Project URL - Add Your Project Url Here
+      url: "http://www.sbi-ecuador-colombia.info/sbiec/ind_estd/info_general/info_gen.html",
+    },
+    {
+      title: "SNI", //Project Title - Add Your Project Title Here
+      para:
+        "SNI (Information National System for Ecuador): ONU Wining award for the comunity service.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://wallpapercave.com/wp/wp2885468.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "http://sni.gov.ec",
+    },
+    {
+      title: "Oculus Testing and Publishing", //Project Title - Add Your Project Title Here
+      para:
+        "VRC (Virtual Reality Checks) for Testing and Publishing at Oculus store for GO and Quest devices", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://mackeysaturday.com/wp-content/uploads/2017/04/oculus_img_1.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.oculus.com/experiences/go",
+    },
+    {
+      title: "Public Companies Dashboard EC", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.hipwallpaper.com%2Fi%2F63%2F89%2FL2trqC.jpg&f=1&nofb=1",
+      //Project URL - Add Your Project Url Here
+      url: "https://appcloud.sni.gob.ec/informacion/indicadores_ep/",
+    },
+    {
+      title: "Oil price and currency quotation", //Project Title - Add Your Project Title Here
+      para:
+        "Information regarding the daily price of a Barrel of Oil; daily oil production in thousands of barrels; quotes of the dollar in relation to other types of currencies; international Libor and Prime rates, and Eurobond prices.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seia.org%2Fsites%2Fdefault%2Ffiles%2Fmedia%2Fstock-footage-world-stock-market-animation.jpg&f=1&nofb=1",
+      //Project URL - Add Your Project Url Here
+      url: "https://sni.gob.ec/cotizacion",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Web Development, Coding & Programming, Search Engine Optimization, Cross-Browser Compatibility, Troubleshooting & Testing",
+  aboutParaTwo:
+    "UX & UI Design, Agile Methodologies, Angular, PWA apps, OCULUS VR Testing/Publishing.",
+  aboutParaThree:
+    "",
+  aboutImage:
+    "https://i.ibb.co/4ZBfjSP/yo.jpg",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "HTML 5",
+    },
+    {
+      img: cssIcon,
+      para:
+        "CSS",
+    },
+    {
+      img: jsIcon,
+      para:
+        "JS",
+    },
+    {
+      img: reactIcon,
+      para:
+        "React",
+    },
+    {
+      img: designIcon,
+      para:
+        "Custom Design",
+    },
+    {
+      img: codeIcon,
+      para:
+        "Coding & Testing",
+    },
+
+    
+
+  ],
+
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  //promotionHeading: "Heading",
+  //promotionPara:
+   // "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
